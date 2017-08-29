64eaeedbd2f57f87e0deae376917378ac49585e4
Junior <junior.moran@tecsup.edu.pe>
2017-08-29 11:10:38 -0500

    TercerCommit

---
+++ lab3Moran/src/main/java/com/tecsup/lab3Moran/StudentReport.java
@@ -0,0 +1,10 @@
+package com.tecsup.lab3Moran;
+
+public class StudentReport {
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Prueba de branchs" );
+        
+    }
+}
