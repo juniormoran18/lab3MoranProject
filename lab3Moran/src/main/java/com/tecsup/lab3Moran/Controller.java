056e8c40ed6b0c73bf8f6df788db0b6032ee3d4f
JF <a>
2017-08-29 09:31:39 -0500

    el pack

--- lab3Moran/src/main/java/com/tecsup/lab3Moran/Controller.java
+++ lab3Moran/src/main/java/com/tecsup/lab3Moran/Controller.java
@@ -8,6 +8,10 @@ public class Controller
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World Proyecto de Jorge!" );
+        showMessage();
+    }
+    public static void showMessage(){
+    	System.out.println("Holaa, hago cambios :V");
     }
 }
