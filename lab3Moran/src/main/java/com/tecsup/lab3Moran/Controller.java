c364ff200afb7676d2b7cb456e292b65216ab783
Junior <junior.moran@tecsup.edu.pe>
2017-08-29 09:24:00 -0500

    PrimerCommit

---
+++ lab3Moran/src/main/java/com/tecsup/lab3Moran/Controller.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab3Moran;
+
+/**
+ * Hello world!
+ *
+ */
+public class Controller 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
